7132c360ef60fd206fbf842cdc457b346cea89ba
Fatou NIANG <67634804+fatou267@users.noreply.github.com>
2023-02-10 17:39:09 +0100

    rer

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://fatouniang.000webhostapp.com/'
-    : '/'
+  transpileDependencies: true
+  
 
   //resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js')
 })
